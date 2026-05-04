74f020d016fba25fed9585783a4776834cd1af2c
imadi005 <aditya94727@gmail.com>
2026-05-05 01:09:33 +0530

    rebuild: web with Aastrosphere favicon and loading screen

--- web_build/flutter_bootstrap.js
+++ web_build/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2256990424" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1754474940" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
