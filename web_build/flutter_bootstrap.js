63c31a6d5f6cc7e16847d858837156a322efb73d
imadi005 <aditya94727@gmail.com>
2026-05-05 00:58:21 +0530

    fix: web build without base-href

--- web_build/flutter_bootstrap.js
+++ web_build/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3236539913" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "307950998" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
